9713aeb6e910ba6c5781c694dc874fdae4fc2526
Addeuz <addeus_lhc@hotmail.com>
2019-02-20 14:09:57 +0100

    Fixed the logic of only showing the course names and showing them onces, alot of help from 'benvc' on StackOverflow, solved with a Regular Expression.

--- js/app.js
+++ js/app.js
@@ -15,40 +15,57 @@ UIkit.util.ready(function () {
 		spinnerDiv.setAttribute("uk-spinner","")
 		targetDiv.appendChild(spinnerDiv)
 
-		// Removing it after 1.5s
+		// Removing it after 1.5s and adding all the found 'SUMMARY' strings that probably needs to be changed
 		setTimeout(function () {
 			// Tar bort spinnern
 			targetDiv.removeChild(spinnerDiv)
 
 			// Creating ul to hold suggestion for coursenames
-			var ulItem = document.createElement("ul")
+			let ulItem = document.createElement("ul")
 			targetDiv.appendChild(ulItem)
 
 			// Splits the input to find the ones with SUMMARY to add them to array and
 			// check if it has already been added, then don't add it, otherwise do
-			var inputLines = inputArea.value.split("\n")
+			let inputLines = inputArea.value.split('\n')
 
 			// Checks if it is a valid file
 			if (inputLines[0] == "BEGIN:VCALENDAR") {
-				console.log("Nu kör jag")
-				var summaryArray = []
-				inputLines.forEach(function(line) {
-					if (line.slice(0,7) == "SUMMARY") {
-						if (summaryArray.indexOf(line) == -1) {
-							// if the index of the line can't be found, add it to the array
-							summaryArray.push(line)
-							console.log(line)
-						} else {
-
-						}
+				let summaryArray = []
+				for (let inputLine of inputLines) {
+					if (inputLine.slice(0,7) == "SUMMARY") {
+						// If the line has 'SUMMARY' in the beginning push it to summaryArray
+						summaryArray.push(inputLine)
 					}
-					var liItem = document.createElement("li")
-					liItem.textContent = line
-					ulItem.appendChild(liItem)
+				}
+				// Using ternary operator for when there is no 'Sign' in 'Summary'
+				// Im chaining them for edge cases when sign or moment doesn't exist
+				const lines = summaryArray.map(line => (line.match(/(?:SUMMARY:Kurs.grp: )(.+)(?:Sign:)/))
+												? line.match(/(?:SUMMARY:Kurs.grp: )(.+)(?:Sign:)/)[1]
+												: line.match(/(?:SUMMARY:Kurs.grp: )(.+)(?:Moment:)/)
+												? line.match(/(?:SUMMARY:Kurs.grp: )(.+)(?:Moment:)/)[1]
+												: false
+												)
+
+				// Filtering the values of the array
+				const results = lines.filter((line, i) => {
+					if (!lines.slice(0, i).includes(line)) {
+						return true
+					}
 				})
+
+				// Looping throught the results array to append them as list items
+				for(let result of results) {
+					// Checking if ternery operator form befory returned something or false
+					// if it returned false, I don't want it in the list items
+					if(result) {
+						var liItem = document.createElement('li')
+						liItem.textContent = result
+						ulItem.appendChild(liItem)
+					}
+				}
 				inputArea.setAttribute("readonly", "")
 			} else {
-				console.log("Men inte nu")
+
 				var liItem = document.createElement("li")
 				liItem.textContent = "Du har inte klistrat in en giltig iCal fil, försök igen"
 				ulItem.appendChild(liItem)
