97780c035d033f521a665e7ee857e02534dbdac3
Addeuz <addeus_lhc@hotmail.com>
2019-02-23 11:32:44 +0100

    Fixed div's instead of ul and li items, much easier to handle with css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 UIkit.util.ready(function () {
 
-	var inputArea = document.getElementById("inputArea")
-	var targetDiv = document.getElementById('suggestionList')
+	let inputArea = document.getElementById("inputArea")
+	let targetDiv = document.getElementById('courseList')
 	inputArea.addEventListener("paste", event => {
 		// Removing all the items left in the list if something gets pasted again
 		while(targetDiv.firstChild) {
@@ -10,29 +10,25 @@ UIkit.util.ready(function () {
 
 		// Adding a spinner after a paste has been done in inputArea for visual effects,
 		// and letting the whole file load
-		var spinnerDiv = document.createElement("div")
+		let spinnerDiv = document.createElement("div")
 		spinnerDiv.className = "uk-align-center"
 		spinnerDiv.setAttribute("uk-spinner","")
 		targetDiv.appendChild(spinnerDiv)
 
 		// Removing it after 1.5s and adding all the found 'SUMMARY' strings that probably needs to be changed
 		setTimeout(function () {
-			// Tar bort spinnern
+			// Remove the spinner
 			targetDiv.removeChild(spinnerDiv)
-
-			// Creating ul to hold suggestion for coursenames
-			let ulItem = document.createElement("ul")
-			targetDiv.appendChild(ulItem)
 
 			// Splits the input to find the ones with SUMMARY to add them to array and
 			// check if it has already been added, then don't add it, otherwise do
 			let inputLines = inputArea.value.split('\n')
 
 			// Checks if it is a valid file
-			if (inputLines[0] == "BEGIN:VCALENDAR") {
+			if (inputLines[0] == 'BEGIN:VCALENDAR') {
 				let summaryArray = []
 				for (let inputLine of inputLines) {
-					if (inputLine.slice(0,7) == "SUMMARY") {
+					if (inputLine.slice(0,7) == 'SUMMARY') {
 						// If the line has 'SUMMARY' in the beginning push it to summaryArray
 						summaryArray.push(inputLine)
 					}
@@ -52,35 +48,93 @@ UIkit.util.ready(function () {
 						return true
 					}
 				})
-
+			    if (results) {
+					let h2Item = document.createElement('h2')
+					h2Item.classList.add('uk-heading-secondary')
+					h2Item.innerHTML = 'Hittade kurser'
+					targetDiv.appendChild(h2Item)
+				}
 				// Looping throught the results array to append them as list items
 				for(let result of results) {
-					// Checking if ternery operator form befory returned something or false
+					// Checking if ternery operator from before returned something or false
 					// if it returned false, I don't want it in the list items
 					if(result) {
-						var liItem = document.createElement('li')
-						liItem.textContent = result
-						ulItem.appendChild(liItem)
-					}
-				}
-				inputArea.setAttribute("readonly", "")
-			} else {
 
-				var liItem = document.createElement("li")
-				liItem.textContent = "Du har inte klistrat in en giltig iCal fil, försök igen"
-				ulItem.appendChild(liItem)
-			}
+						let resultIndex = results.indexOf(result)
+
+						// One div DOM element per result
+						let courseListItemDiv = document.createElement('div')
+						courseListItemDiv.setAttribute('uk-grid','')
+						courseListItemDiv.setAttribute('id','courseListItem')
+						courseListItemDiv.classList.add('uk-width-1-1', 'uk-padding-small', 'uk-padding-remove-left', 'uk-margin-remove-top')
+						courseListItemDiv.style.borderTop = '1px solid #e5e5e5'
+						targetDiv.appendChild(courseListItemDiv)
+
+						let courseNameSpanDiv = document.createElement('div')
+						courseNameSpanDiv.classList.add('uk-width-5-6')
+						courseListItemDiv.appendChild(courseNameSpanDiv)
+
+						let courseNameSpan = document.createElement('span')
+						courseNameSpan.classList.add('uk-flex', 'uk-flex-middle', 'uk-height-1-1')
+						courseNameSpan.appendChild(document.createTextNode(result))
+						courseNameSpanDiv.appendChild(courseNameSpan)
+
+						let courseEditButtonDiv = document.createElement('div')
+						courseEditButtonDiv.classList.add('uk-width-1-6')
+						courseListItemDiv.appendChild(courseEditButtonDiv)
+
+						let courseEditButton = document.createElement('button')
+						courseEditButton.setAttribute('id', 'editButton'+resultIndex)
+						courseEditButton.setAttribute('uk-toggle', 'target: #editDiv'+resultIndex)
+						courseEditButton.classList.add('uk-button', 'uk-button-default', 'uk-align-right', 'uk-margin-remove')
+						courseEditButton.innerHTML = 'Redigera'
+						courseEditButtonDiv.appendChild(courseEditButton)
+
+						let courseEditNameDiv = document.createElement('div')
+						courseEditNameDiv.setAttribute('uk-grid', '')
+						courseEditNameDiv.setAttribute('hidden', '')
+						courseEditNameDiv.setAttribute('id', 'editDiv'+resultIndex)
+						courseEditNameDiv.classList.add('uk-margin-small-top')
+						courseListItemDiv.appendChild(courseEditNameDiv)
+
+						let editInputNameDiv = document.createElement('div')
+						editInputNameDiv.classList.add('uk-width-1-2')
+						courseEditNameDiv.appendChild(editInputNameDiv)
+
+						let editInputName = document.createElement('input')
+						editInputName.setAttribute('id', 'editInput'+resultIndex)
+						editInputName.setAttribute('type', 'text')
+						editInputName.classList.add('uk-input')
+						editInputName.style.width = "1000px"
+						editInputNameDiv.appendChild(editInputName)
+
+						let editSaveButtonDiv = document.createElement('div')
+						editSaveButtonDiv.classList.add('uk-width-1-2')
+						courseEditNameDiv.appendChild(editSaveButtonDiv)
+
+						let editSaveButton = document.createElement('button')
+						editSaveButton.setAttribute('id', 'saveEditButton'+resultIndex)
+						editSaveButton.classList.add('uk-button', 'uk-button-primary', 'uk-margin-remove')
+						editSaveButton.innerHTML = 'Spara'
+						editSaveButtonDiv.appendChild(editSaveButton)
+						}
+					}
+					inputArea.setAttribute('readonly', '')
+				} else {
+					let h4Item = document.createElement('h4')
+					h4Item.innerHTML = 'Du har ej klistrat in en giltig .ics fil, var vänlig försök igen'
+					targetDiv.appendChild(h4Item)
+				}
 		}, 1500)
 	})
 	// Clear the text area and all the examples
-	var clearButton = document.getElementById('clearAreaButton')
-	clearButton.addEventListener("click", event => {
-		inputArea.value = ""
+	let clearButton = document.getElementById('clearAreaButton')
+	clearButton.addEventListener('click', event => {
+		inputArea.value = ''
 		// Removing all the items left in the list if something gets pasted again
 		while(targetDiv.firstChild) {
 			targetDiv.removeChild(targetDiv.firstChild)
 		}
-		inputArea.removeAttribute("readonly")
+		inputArea.removeAttribute('readonly')
 	})
 })
-
